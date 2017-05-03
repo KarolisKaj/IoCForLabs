3171bc797134dd72d6d9bb8212b98af1d4308589
Karolis Kajėnas <karolis.kajenas1@gmail.com>
2017-05-03 22:42:22 +0300

    Init Unity

---
+++ CSharp/IoCUnity/IoCUnity/UnityAttributes.cs
@@ -0,0 +1,14 @@
+﻿namespace IoCUnity
+{
+    using Microsoft.Practices.Unity;
+    using System.Linq;
+
+    public class UnityAttributes
+    {
+        public void Start()
+        {
+            IUnityContainer myContainer = new UnityContainer();
+            myContainer.RegisterTypes(AllClasses.FromLoadedAssemblies().Where(x => x.Assembly.FullName.Contains("IoCAttributes")));
+        }
+    }
+}
